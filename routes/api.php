0cc01b738cac06fb52d015b7085a50ac014d40e8
Lehatran <leha.tran277@gmail.com>
2024-11-28 17:06:41 +0700

    bai thuc hanh 2

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HelloWorldController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('HelloWorld', [HelloWorldController::class, 'HelloWorld']);
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('HelloWorld', [HelloWorldController::class, 'HelloWorld'])->middleware('api.auth');
+    Route::post('logout', [AuthController::class, 'logout'])->name('api.auth.logout');
+    Route::post('refresh', [AuthController::class, 'refresh'])->middleware('api.auth');
+    Route::get('profile', [AuthController::class, 'profile'])->middleware('api.auth');
+});
